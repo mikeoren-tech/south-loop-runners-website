be495bf765ee41a7fef4574e65a801cf9fa9c144
mikeoren-tech <mikeoren@gmail.com>
2025-11-10 11:01:01 -0600

    Update upcoming-runs.tsx
    Darker background

--- components/upcoming-runs.tsx
+++ components/upcoming-runs.tsx
@@ -139,7 +139,7 @@ function PaceInterestSection({ runId, hasSocial }: { runId: string; hasSocial: b
             <SelectTrigger className="flex-1">
               <SelectValue placeholder="Select your pace" />
             </SelectTrigger>
-            <SelectContent className="glass-strong">
+            <SelectContent className="glass dark:bg-black/80">
               {PACE_GROUPS.map((pace) => (
                 <SelectItem key={pace} value={pace}>
                   {pace}
